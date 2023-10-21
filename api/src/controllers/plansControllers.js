a8003c666c1663c9b612c93d514bdcc6e12e9a92
Leonardo E Micale <124411705+LeonardoEMicale@users.noreply.github.com>
2023-10-21 04:07:57 -0300

    Update plansControllers.js

--- api/src/controllers/plansControllers.js
+++ api/src/controllers/plansControllers.js
@@ -1,7 +1,7 @@
 const mercadopago = require('mercadopago');
 
 mercadopago.configure({
-    access_token: 'TEST-8033250124943304-100419-9f5940470a6ee188e569925fef444342-1500823282'
+    access_token: '***********************************'
 })
 
 const plansController = (req, res) => {
